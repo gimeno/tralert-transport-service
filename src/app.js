3c35ceefc71c5fa9b0350e319b43779f963a18cb
Roberto Gimeno <rgimeno@guidewire.com>
2020-04-18 17:35:44 +0200

    Add common middlewares

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const compression = require('compression');
+const cors = require('cors');
 const { env } = require('./config/config');
 const morgan = require('./config/morgan');
 
@@ -8,6 +12,25 @@ if (env !== 'test') {
     app.use(morgan.successHandler);
     app.use(morgan.errorHandler);
 }
+
+// set security HTTP headers
+app.use(helmet());
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(xss());
+
+// gzip compression
+app.use(compression());
+
+// enable CORS - Cross Origin Resource Sharing
+app.use(cors());
+app.options('*', cors());
 
 app.use('/', (req, res) => {
     res.send('Working');
