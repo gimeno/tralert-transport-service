ff1e883bf2a42a5dae85169347a287203c434fb3
gimeno_r <r.gimenocallejas@gmail.com>
2020-04-24 20:49:11 +0200

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e # Stop script from running if there are any errors
+
 environment=${1}
 
 # Log in to Docker Hub
@@ -13,3 +13,5 @@ GIT_VERSION=$(git describe --always --abbrev --tags --long)
 docker build -t ${IMAGE}:${GIT_VERSION} .
 docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:${environment}
 docker push ${IMAGE}:${GIT_VERSION}
+
+exit 0
