8b4073d67ad3f85532597f2a30006eefa485d4bb
gimeno_r <r.gimenocallejas@gmail.com>
2020-04-25 10:48:27 +0200

    Update deploy script to use docker to deploy in Heroku

--- deploy.sh
+++ deploy.sh
@@ -16,4 +16,23 @@ docker push ${IMAGE}:${GIT_VERSION}
 docker tag service ${IMAGE}:${environment}
 docker push ${IMAGE}:${environment}
 
+# If env is dev also deploy to Heroku
+if [ "${environment}" == "development" ]
+then
+
+    # Get Heroku cli
+    wget -qO- https://toolbelt.heroku.com/install.sh | sh
+
+    # Login into Heroku registry
+    echo "$HEROKU_PASSWORD" | docker login -u "$HEROKU_USERNAME" --password-stdin registry.heroku.com
+
+    # Tag and push image to heroku registry
+    docker tag service registry.heroku.com/$HEROKU_APP_NAME/web
+    docker push registry.heroku.com/$HEROKU_APP_NAME/web;
+
+    # Release image in app
+    heroku container:release web --app $HEROKU_APP_NAME
+
+fi
+
 exit 0
