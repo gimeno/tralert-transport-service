a354e3674092a3c2523abc7e8a0a2c2988b2c550
gimeno_r <r.gimenocallejas@gmail.com>
2020-04-25 18:43:08 +0200

    Add logger transport to centralize logs

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,8 @@ const envVarsSchema = Joi.object()
     .keys({
         NODE_ENV: Joi.string().valid('production', 'development', 'test').default('production'),
         PORT: Joi.number().default(5000),
-        CHROMIUM_PATH: Joi.string().default('')
+        CHROMIUM_PATH: Joi.string().default(''),
+        LOGS_TOKEN: Joi.string().when('NODE_ENV', { is: 'production', then: Joi.required(), otherwise: Joi.optional() })
     })
     .unknown();
 
@@ -17,5 +18,6 @@ if (error) {
 module.exports = {
     env: envVars.NODE_ENV,
     port: envVars.PORT,
-    chromiumPath: envVars.CHROMIUM_PATH
+    chromiumPath: envVars.CHROMIUM_PATH,
+    logsToken: envVars.LOGS_TOKEN
 };
